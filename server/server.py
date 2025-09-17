27b11ba4c0907824ee9394eb577808124b4f3acd
Ryan Brady <rbrady.business@gmail.com>
2025-09-17 03:23:41 -0400

    continuous loop and ack

--- server/server.py
+++ server/server.py
@@ -3,32 +3,43 @@ import json
 import time
 
 async def handle_client(reader, writer):
-    
-    # take one input line, as utf-8, cut off the newline, and print as a python string
-    data = await reader.readline()
-    if not data:
-        return
-    line = data.decode("utf-8", errors="replace").rstrip("\n")
-    print(f"RAW: {line}")
+    nick = "anon" # per-connection state
 
+    while True: 
+        data = await reader.readline()
+        if not data: # close client
+            break
+        line = data.decode("utf-8", errors="replace").rstrip("\n")
+        print(f"RAW: {line}")
+        # try to turn incoming JSON into python dict
+        # if it fails, send error, if it works, send acknowledgment
+        try:
+            obj = json.loads(line)
+        except json.JSONDecodeError:
+            err = {"type": "error", "text": "invalid json"}
+            writer.write((json.dumps(err) + "\n").encode("utf-8")) # queue up bytes to the socket
+            await writer.drain() # flush the queue
+            writer.close()
+            await writer.wait_closed()
+            continue
+        
+        t = obj.get("type")
+        if t == "nick":
+            nick = obj.get("nick", "anon")
+            reply = {"type": "notice", "text": f"nick = {nick}", "ts": int(time.time())}
+        elif t == "msg":
+            text = obj.get("text", "")
+            print(f"{nick}: {text}")
+            reply = {"type": "notice", "text": "ok", "ts": int(time.time())}
+        else:
+            reply = {"type": "error", "text": "invalid message type", "ts": int(time.time())}
 
-    # try to turn incoming JSON into python dict
-    # if it fails, send error, if it works, send acknowledgment
-    try:
-        obj = json.loads(line)
-    except json.JSONDecodeError:
-        err = {"type": "error", "text": "invalid json"}
-        writer.write((json.dumps(err) + "\n").encode("utf-8")) # queue up bytes to the socket
-        await writer.drain() # flush the queue
-        writer.close()
-        await writer.wait_closed()
-        return # close cleanly after one message
+        writer.write((json.dumps(reply) + "\n").encode("utf-8"))
+        await writer.drain()
 
-    ack = {"type": "notice", "text": "ok", "ts": int(time.time())}
-    writer.write((json.dumps(ack) + "\n").encode("utf-8"))
-    await writer.drain()
     writer.close()
     await writer.wait_closed()
+
 
 
 async def main():
